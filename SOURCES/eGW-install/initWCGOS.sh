27d216cc48295cf7e2c4d872c49410ced8084887
dongfeng <dongfeng@baicells.com>
2021-08-30 02:59:08 +0000

    更新 'SOURCES/eGW-install/initWCGOS.sh'

--- SOURCES/eGW-install/initWCGOS.sh
+++ SOURCES/eGW-install/initWCGOS.sh
@@ -2,19 +2,21 @@
 
 systemctl daemon-reload
 systemctl stop NetworkManager.service && systemctl disable NetworkManager.service
+systemctl stop firewalld.service && systemctl disable firewalld.service
 systemctl restart om.service && systemctl enable om.service
-[[ -d /root/eGW  ]] && rm -rf /root/eGW
-ln -sf /home/wcg/eGW /root/eGW
+
 [ ! -d "/var/opc/lc"  ] && mkdir -p /var/opt/lc
 [ ! -d "/var/opc/mo"  ] && mkdir -p /var/opt/mo
 
-systemctl stop firewalld.service && systemctl disable firewalld.service
+
 ulimit -c unlimited
 setenforce 0
 sed -i "s/SELINUX=enforcing/SELINUX=disabled/g"  /etc/selinux/config
+
 sed -i "s/#Port.*/Port 50683/g" /etc/ssh/sshd_config
 sed -i 's/#\(PermitRootLogin \).*/\1no/g' /etc/ssh/sshd_config
 systemctl restart sshd.service
+
 grep "export TMOUT" /etc/profile 2>&1>/dev/null || echo "export TMOUT=300" >> /etc/profile
 sed -i 's/\(.*pam_pwquality.so \).*/\1retry=3 ucredit=-1 dcredit=-1 ocredit=-1/g' /etc/pam.d/system-auth
 sed -i '/required/s@#auth\(.*\)@auth\1@g' /etc/pam.d/su
@@ -80,8 +82,8 @@ grep "kernel.msgmnb" /etc/sysctl.conf 2>&1>/dev/null && \
 sed -i "s/.*\(kernel.msgmnb\).*/\1 = 4203520/g"  /etc/sysctl.conf || \
 echo "kernel.msgmnb = 4203520" >> /etc/sysctl.conf
 sysctl -p
-[ ! -d "/root/coredump"  ] && mkdir -p /root/coredump && chmod 0773 /root/coredump
-echo "kernel.core_pattern = /root/coredump/core-%e-sig%s-user%u-group%g-pid%p-time%t" > /etc/sysctl.d/core.conf
+[ ! -d "/var/log/eGW/coredump"  ] && mkdir -p /var/log/eGW/coredump && chmod 0773 /var/log/eGW/coredump
+echo "kernel.core_pattern = /var/log/eGW/coredump/core-%e-sig%s-user%u-group%g-pid%p-time%t" > /etc/sysctl.d/core.conf
 echo "kernel.core_uses_pid = 1" >> /etc/sysctl.d/core.conf
 echo "fs.suid_dumpable = 2" >> /etc/sysctl.d/core.conf
 echo "*       hard        core        unlimited" > /etc/security/limits.d/core.conf
@@ -95,9 +97,9 @@ echo "DefaultLimitNOFILE=102400" >> /etc/systemd/system.conf
 systemctl daemon-reload
 systemctl daemon-reexec
 sysctl -p /etc/sysctl.d/core.conf
-[ ! -d /root/eGW/Logs/history ] && mkdir -p /root/eGW/Logs/history
+[ ! -d /var/log/eGW/history ] && mkdir -p /var/log/eGW/history
 file_cfg='/etc/bashrc'
-prompt_command_format='if [[ $(who am i) == "root" ]];then { date "+%Y-%m-%d %T ##### $(who am i |awk "{print \$1\" \"\$2\" \"\$5}") #### $(pwd) #### $(history 1 | { read x cmd; echo "$cmd"; })"; } >> /root/eGW/Logs/history/$(date +"%Y%m%d").log &> /dev/null;fi'
+prompt_command_format='if [[ $(whoami) == "root" ]];then { date "+%Y-%m-%d %T ##### $(who am i |awk "{print \$1\" \"\$2\" \"\$5}") #### $(pwd) #### $(history 1 | { read x cmd; echo "$cmd"; })"; } >> /var/log/eGW/history/$(date +"%Y%m%d").log 2> /dev/null;fi'
 sed -i "/.*\(export PROMPT_COMMAND=\).*/d" $file_cfg
 echo "export PROMPT_COMMAND='$prompt_command_format'" >> $file_cfg
 [ ! -d "/var/log/journal"  ] && mkdir -p /var/log/journal
